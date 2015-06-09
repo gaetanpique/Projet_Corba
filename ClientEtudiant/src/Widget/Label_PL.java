d62fed7049cf30788cc66777b40ce101f80b63bd
T3836883 <T3836883@LT040926.za.if.atcsg.net>
2015-06-09 22:40:55 +0200

    commit du composeurConnexion (IHM connexion) plus commit des packages widget

---
+++ ClientEtudiant/src/Widget/Label_PL.java
@@ -0,0 +1,24 @@
+package Widget;
+import org.eclipse.swt.widgets.*;
+
+/**
+ * 
+ * @author T3836883
+ *
+ */
+public class Label_PL extends Label
+{
+
+	public Label_PL(Composite arg0, int arg1) 
+	{
+		
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+		
+	}
+	
+	public void checkSubclass()
+	{
+		
+	}
+}
