4af58abd85076a50db5fb99809a1ddaee4e8c8d2
BAPeTISTE <BAPeTISTE@BAPTISTE>
2015-06-14 19:46:50 +0200

    test

---
+++ GestionMinistere/src/Ministere/FormationImpl.java
@@ -0,0 +1,32 @@
+package Ministere;
+
+import Etudes.Formation;
+import Etudes.FormationPOA;
+
+ class FormationImpl extends FormationPOA {
+
+	 private String intitule;
+	 
+	 public FormationImpl(String intit){
+		 this.intitule = intit;
+	 }
+	 
+	@Override
+	public String intitule() {
+		return this.intitule;
+	}
+
+
+	/**
+	 * Cette méthode compare les intitulés de la formation et de celle passée en paramètre
+	 * 
+	 * @return true : intitulés identiques / false : intitulés différents
+	 * @author Baptiste
+	 */
+	@Override
+	public boolean _equals(Formation formation) {
+		return (this.intitule().equals(formation.intitule()));
+	}
+
+	
+}
